c7b162e7fad3a50d9f9bfbb1d90d4db739ef44ea
cheng5 <superduchengwu@163.com>
2021-07-08 22:51:50 +0800

    init: init and add header and footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Layout, Typography } from 'antd';
+
+export const Footer: React.FC = () => {
+  return (
+    <Layout.Footer>
+      <Typography.Title level={3} style={{ textAlign: 'center' }}>
+        版权所有 @ cheng5.MUTU
+      </Typography.Title>
+    </Layout.Footer>
+  )
+}
